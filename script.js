a47f3374d5630e4d6c4327ac3752a0aa2f401d58
adarshhlohar <adarshhlohar@gmail.com>
2024-01-04 22:33:11 +0530

    we have created method in js we can validate email

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ var salaryInput = document.getElementById('salary');
 
 
 const submit = document.getElementById("submit");
-const name = document.getElementById("name")
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+
 const nameError = document.getElementById("nameError");
 
 name.addEventListener("blur", () => {
@@ -26,3 +28,22 @@ name.addEventListener("blur", () => {
         nameError.innerText = "First name starts with Cap and has minimum 3 characters";
     }
 });
+
+email.addEventListener("blur", ()=>{
+    console.log("The pwd has been blured");
+    let regex = /^[a-z]{3,9}\.[a-z]{0,}\@bl\.co\.?i?n?/;
+    let str = email.value;
+
+    if (regex.test(str)) {
+        console.log(`This is valid ${str}`);
+        submit.removeAttribute("disabled");
+        emailError.style.display = "none";
+    }else{
+        console.log(`This not valid ${str}`);
+        submit.setAttribute("disabled","true");
+        emailError.innerText = "Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) with precise @ and . positions";
+        emailError.style.display = "block";
+    }
+});
+
+
